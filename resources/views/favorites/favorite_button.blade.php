5b390a405b2c823804dbddca2b0a07899c46eeda
syunin_9 <moritan_014029d.ne@outlook.jp>
2020-05-16 13:57:20 +0000

    favorite

---
+++ resources/views/favorites/favorite_button.blade.php
@@ -0,0 +1,11 @@
+@if (Auth::id() != $micropost->id)
+    @if (Auth::user()->is_favorite($micropost->id))
+        {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+            {!! Form::submit('unfavorite', ['class' => "btn btn-danger btn-sm"]) !!}
+        {!! Form::close() !!}
+    @else
+        {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
+            {!! Form::submit('favorite', ['class' => "btn btn-primary btn-sm"]) !!}
+        {!! Form::close() !!}
+    @endif
+@endif
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -9,6 +9,7 @@
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
+                @include('favorites.favorite_button', ['microposts' => $microposts])
                 <div>
                     @if (Auth::id() == $micropost->user_id)
                         {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
